912a4410211021a689c952b83ca3e5382d47d2ef
Naresh M <naresh2004.m@gmail.com>
2023-09-21 19:24:58 +0530

    section 10 completed

--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
-    {/* <StarRating maxRating={10} />
-    <StarRating /> */}
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating
+      maxRating={5}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
